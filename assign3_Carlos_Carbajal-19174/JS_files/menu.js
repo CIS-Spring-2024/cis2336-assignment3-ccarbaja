442bb4986e2d69e2c02dbcb9b33ac1da605a4f35
ccarbaja <ccarbaja@cougarnet.uh.edu>
2024-04-12 05:24:40 -0500

    added update to order totals

--- assign3_Carlos_Carbajal-19174/JS_files/menu.js
+++ assign3_Carlos_Carbajal-19174/JS_files/menu.js
@@ -9,10 +9,65 @@ const breakfastmenu = document.querySelector("#breakfastmenu");
 const veganbtn= document.querySelector("#veganbtn");
 const veganmenu = document.querySelector("#veganmenu");
 
-const orderbtn = document.querySelector(".orderbtn");
+const burritobtn = document.querySelector("#burritobtn");
+const omelettebtn = document.querySelector("#omelettebtn");
+const chickenwafflesbtn = document.querySelector("#chickenwafflesbtn");
+const saladbtn = document.querySelector("#saladbtn");
+const joebtn = document.querySelector("#joebtn");
+const cheeseburgerbtn = document.querySelector("#cheeseburgerbtn");
+const farmercharmbtn = document.querySelector("#farmercharmbtn");
+const pizzabtn = document.querySelector("#pizzabtn");
+const tofubtn = document.querySelector("#tofubtn");
+const alfredobtn = document.querySelector("#alfredobtn");
+const lasagnabtn = document.querySelector("#lasagnabtn");
+const spaghettibtn = document.querySelector("#spaghettibtn");
 
-orderbtn.addEventListener('click', () => {
-    alert("Successfully added to order!");
+/*const burritoquantity = document.querySelector("#burritoquantity");*/
+const omelettequantity = document.querySelector("#omelettequantity");
+const chickenwafflesquantity = document.querySelector("#chickenwafflesquantity");
+const saladquantity = document.querySelector("#saladquantity");
+const joequantity = document.querySelector("#joequantity");
+const cheeseburgerquantity = document.querySelector("#cheeseburgerquantity");
+const farmercharmquantity = document.querySelector("#farmercharmquantity");
+const pizzaquantity = document.querySelector("#pizzaquantity");
+const tofuquantity = document.querySelector("#tofuquantity");
+const alfredoquantity = document.querySelector("#alfredoquantity");
+const lasagnaquantity = document.querySelector("#lasagnaquantity");
+const spaghettiquantity = document.querySelector("#spaghettiquantity");
+
+let orderTotal = 0;
+let tax = .0625;
+let taxTotal=0;
+burritobtn.addEventListener('click', () => {
+    const burritoquantity = document.querySelector("#burritoquantity");
+    quantity = burritoquantity.value;
+    orderTotal+=(quantity * 8.99)+((quantity * 8.99)* tax);
+    taxTotal= "Tax: $" + ((quantity * 8.99)* tax);
+    newTotal = "Total: $" + orderTotal.toFixed(2);
+    document.querySelector("#ordertotal").textContent=newTotal; 
+    document.querySelector("#taxtotal").textContent=taxTotal;
+
+});
+
+omelettebtn.addEventListener('click', () => {
+    const omelettequantity = document.querySelector("#omelettequantity");
+    quantity = omelettequantity.value;
+    orderTotal+=(quantity * 10.99)+((quantity * 10.99)* tax);
+    newTotal = "Total: $" + orderTotal.toFixed(2);
+    taxTotal= "Tax: $" + ((quantity * 10.99)* tax);
+    document.querySelector("#ordertotal").textContent=newTotal;
+    document.querySelector("#taxtotal").textContent=taxTotal; 
+
+});
+
+chickenwafflesbtn.addEventListener('click', () => {
+    const chickenwafflesquantity = document.querySelector("#chickenwafflesquantity");
+    quantity = chickenwafflesquantity.value;
+    orderTotal+=(quantity * 13.99)+((quantity * 13.99)* tax);
+    newTotal = "Total: $" + orderTotal.toFixed(2);
+    taxTotal= "Tax: $" + ((quantity * 13.99)* tax);
+    document.querySelector("#ordertotal").textContent=newTotal;
+    document.querySelector("#taxtotal").textContent=taxTotal; 
 
 });
 
